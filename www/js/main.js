9f6b988c63cf1a6e290156aa3e0394d4ab1ca6ee
Azathoth <racinsky.matej@seznam.cz>
2016-03-25 01:33:16 +0100

    hopefully working ajax refreshing

--- www/js/main.js
+++ www/js/main.js
@@ -1,23 +1,18 @@
-//$(function () {
-//    console.log('preparing to init');
-    //$.nette.init();
-    //
-    //console.log('preparing to reload');
-    //if(reload) {
-    //    console.log('reload');
-    //    setInterval(function() {
-    //        console.log('realoding');
-    //        $.nette.ajax({
-    //            'url': '?do=redraw',
-    //            'data': {
-    //            }
-    //        });
-    //    }, 5000)
-    //} else {
-    //    console.log('not reload');
-    //}
-//});
+$(function () {
+    $.nette.init();
 
-$(function() {
-    console.log( "ready!" );
+    console.log('preparing to reload');
+    if(reload) {
+        console.log('reload');
+        setInterval(function() {
+            console.log('realoding');
+            $.nette.ajax({
+                'url': '?do=redraw',
+                'data': {
+                }
+            });
+        }, 5000)
+    } else {
+        console.log('not reload');
+    }
 });
